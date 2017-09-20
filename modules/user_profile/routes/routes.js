a0e41bf46b4313821af8b1981d5c36dd21722273
Jitendra <jitendra@finoit.co.in>
2017-09-20 14:48:33 +0530

    remove classes from user account

--- modules/user_profile/routes/routes.js
+++ modules/user_profile/routes/routes.js
@@ -1,4 +1,5 @@
 const path			= require('path'),
+	// _ 				= require('lodash')
 	fs 				= require('fs'),
 	router 			= require('express').Router(),
 	dir 			= `${path.dirname(__dirname)}/controllers`,
@@ -9,11 +10,19 @@ let ReadDirectory 	= new helperLib.read_directory.readDirectory()
 
 const fileObj 		= ReadDirectory.requireFiles(dir)
 
+function decodeToken(req, res, next) {
+		let Jwt = new helperLib.jwt.jwt(),
+		 authorization = req.headers.authorization.replace('Bearer ', ''),
+		 decodeToken = Jwt.verify(authorization);
+		 req.tokenInfo = decodeToken
+		next();
+}
+
 router
-	.post('/register', 						fileObj.login_register.loginRegister.register)
-	.put('/update', 						fileObj.login_register.loginRegister.update)
-	.post('/login', 						fileObj.login_register.loginRegister.login)
-	.put('/change-password', 				fileObj.password.password.changePassword)
+	.post('/register', 						fileObj.login_register.register)
+	.put('/update', 						fileObj.login_register.updateProfile)
+	.post('/login', 						fileObj.login_register.login)
+	.put('/change-password',  decodeToken, 	fileObj.login_register.changePassword)
 	.put('/forgot-password', 				fileObj.password.password.forgotPassword)
 
 
