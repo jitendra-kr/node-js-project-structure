8a5c52f56e9d69aa9b29159043b2b28152d2b08d
monis01 <mnskhn7997@gmail.com>
2017-09-24 19:30:30 +0530

    validate password middleware updated

--- config/lib/helper_lib/middleware.js
+++ config/lib/helper_lib/middleware.js
@@ -33,18 +33,31 @@ class Middleware {
 			if (length < minLength || length > maxLength || havingSpace || !havingSpecialChar) {
 	            
 	            resObj.status = 'failed' ;
-	            resObj.statusCode = 200  ;
-	            resObj.hint = 'Password@' ;
-	            resObj.message = length < minLength  
-	            				? 'password minimum length should be 8'
-	            				: length > maxLength
-	            				? 'password maximum length should be 12'
-	            				: havingSpace
-	            				? 'password should not contain any space'
-	            				: 'password should have one special character'
+	            resObj.statusCode = 400  ; //@ failed status code is never 200 
+	         
+	         	//@ password hint should be shown on front end and to be handled there only, 
+	         	//@ server should not be dicating the password hints
+	         	//@ server responsonble for only validation and not suggestions
+	            //resObj.hint = 'Password@' ; 
+
+	            //@ instead of 
+	            // resObj.message = length < minLength  
+	            // 				? 'password minimum length should be '+minLength
+	            // 				: length > maxLength
+	            // 				? 'password maximum length should be '+maxLength
+	            // 				: havingSpace
+	            // 				? 'password should not contain any space'
+	            // 				: 'password should have one special character'
+
+	            //@ can be
+	            if(length < minLength || length > maxLength){
+	            	resObj.message = "password length shoud be not be less than " + minLength + " and should not be greater than " +maxLength ;
+	            }else if (havingSpace || !havingSpecialChar){
+	            	resObj.message = "password should not contain any spaces or special character ";
+	            }
 
 	            res.status(resObj.statusCode).json(resObj);
-
+	            
 			}else{
 				next() ;		
 			}
