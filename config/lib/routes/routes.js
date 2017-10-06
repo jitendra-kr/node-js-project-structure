caf2098b21ee5e9c426e07464da1fdfd705822f1
Jitendra <jitendra@finoit.co.in>
2017-10-06 18:29:58 +0530

    comment and indenting

--- config/lib/routes/routes.js
+++ config/lib/routes/routes.js
@@ -1,28 +1,25 @@
 const path			     = require('path'),
-	  fs 			         = require('fs'),
-    os               = require('os'),
-    expressJWT       = require('express-jwt'),
-    ENV              = require(path.resolve(`./config/env/${process.env.NODE_ENV}`))
-    helperLib        = require(path.resolve('./config/lib/helper_lib')),
-	  location		     = path.resolve('./modules');
+	fs 			         = require('fs'),
+    os                   = require('os'),
+    expressJWT           = require('express-jwt'),
+    ENV                  = require(path.resolve(`./config/env/${process.env.NODE_ENV}`))
+    helperLib            = require(path.resolve('./config/lib/helper_lib')),
+	location		     = path.resolve('./modules');
 
 
 module.exports = (app) => {
 
     let dirObj = {}
 
-    // validate api with express-jwt
+    //@ validate api with express-jwt
     app.use(expressJWT({
-
-        secret: new Buffer(ENV.JWT_KEY).toString('base64')
-
+        secret: new Buffer(ENV.JWT_KEY).toString('base64');
     }).unless({
-
-        // pass api without validating
+        //@ pass api without validating
         path:['/']
     }));
 
-    // error handler for unauthorized routes
+    //@ error handler for unauthorized routes
     app.use(function (err, req, res, next) {
       if (err.name === 'UnauthorizedError') {
 
@@ -39,39 +36,24 @@ module.exports = (app) => {
 
     fs.readdirSync(location)
         .filter((dir) => {
-
-            return fs.statSync(`${location}/${dir}`).isDirectory() ;
-
+            return fs.statSync(`${location}/${dir}`).isDirectory();
         }).forEach((dir, index) => {
 
-            let fileObj ;
-
-            if (index == 0) {
-                app.get('/', (req, res) => {
-
-                    res.render('index', {
-                        port: 3005
-                    });
-
-                })
-            }
-
-            fileObj = require(path.resolve(`./modules/${dir}/routes/routes`));
-            
+            let fileObj;
+            fileObj = require(path.resolve(`./modules/${dir}/routes/routes`));            
             app.use(fileObj.base,	fileObj.router);
 
         });
 
 
-    // error handling middleware
+    //@ error handling middleware
     app.use((err, req, res, next) => {
 
         let Common        = new helperLib.common.common();
         let Middleware    = new helperLib.middleware();
 
-        // write error logs into file
+        //@ write error logs into file
         Middleware.writeErrorIntoFile(err, req);
-
         let resObj = Common.generateResponses(500, 'failed', err.message || err.stack);
 
         res.status(500).json(resObj);
