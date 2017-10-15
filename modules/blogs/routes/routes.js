d5760dad498f600ccf875b686aa2179df11b2323
monis01 <mnskhn7997@gmail.com>
2017-10-15 16:45:16 +0530

    soft delete blog post

--- modules/blogs/routes/routes.js
+++ modules/blogs/routes/routes.js
@@ -1,4 +1,4 @@
-
+	
 const path			= require('path'),
 	fs 				= require('fs'),
 	router 			= require('express').Router(),
@@ -14,10 +14,11 @@ let fileObj 		= ReadDirectory.requireFiles(dir);
 
 //@ routes mapping
 router
-	.post('/create-blog', 						fileObj['crud.blog'].create)
-	.get('/read-blog', 							fileObj['crud.blog'].read)
-	.put('/update-blog', 						fileObj['crud.blog'].update)
-	.delete('/delete-blog', 					fileObj['crud.blog'].delete);
+	.post('/create-blog', 									fileObj['crud.blog'].create)
+	.get('/read-blog/:blog_id', 							fileObj['crud.blog'].read)
+	.put('/:blog_id/update-blog', 							fileObj['crud.blog'].update)
+	.put('/:blog_id/soft-delete-blog', 						fileObj['crud.blog'].softDelete)
+	.delete('/:blog_id/delete-blog', 						fileObj['crud.blog'].finalDelete);
 
 
 module.exports = {
