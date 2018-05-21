03ec77f6b3fa86ede8851545b6220c22c40ced5a
Jitendra <jitendra@finoit.co.in>
2018-05-21 12:51:37 +0530

    ignored saved response and pass only email and created at

--- modules/user_profile/controllers/user.account.js
+++ modules/user_profile/controllers/user.account.js
@@ -19,8 +19,11 @@ exports.register = (req, res) => {
             let message = err.code == '11000' ? `${req.body.email} ${helperLib.messages.alreadyTaken}` : 'Registration failed';
                 resObj = Common.generateResponses(400, 'failed', message, err.code == '11000'? null : err);
         } else {
-                saved.password = undefined;
-                resObj = Common.generateResponses(200, 'success', helperLib.messages.accoundCreated, null, saved);
+                let result = {
+                    email: saved.email,
+                    created_at: saved.created_at
+                }
+                resObj = Common.generateResponses(200, 'success', helperLib.messages.accoundCreated, null, result);
         }
 
         res.status(resObj.statusCode).json(resObj);
