a71417f3d165c0ac993d7a38e252c3babe73b8c8
KUMAR Jitendra <jitendra.kumar@soprasteria.com>
2019-01-31 11:53:35 +0530

    removed unused package from express.js

--- config/lib/express/express.js
+++ config/lib/express/express.js
@@ -1,36 +1,37 @@
-const helmet			= require('helmet'),
-	  express			= require('express'),
-	  path				= require('path'),
-	  morgan 			= require('morgan'),
-	  chalk				= require('chalk'),
-	  bodyParser 		= require('body-parser');
+const helmet = require("helmet"),
+  express = require("express"),
+  path = require("path"),
+  morgan = require("morgan"),
+  bodyParser = require("body-parser");
 
-module.exports = function (app) {
+module.exports = function(app) {
+  //@ used helmet to secure headers
+  app.use(helmet());
 
-	//@ used helmet to secure headers
-	app.use(helmet());
+  //@ parse application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: false }));
 
-	//@ parse application/x-www-form-urlencoded
-	app.use(bodyParser.urlencoded({ extended: false })) ;
+  //@ parse application/json
+  app.use(bodyParser.json());
 
-	//@ parse application/json
-	app.use(bodyParser.json());
-    
-    //@ allow/enable cross origin request	
-	app.use(function(req, res, next) {
-	    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-	    res.header("Access-Control-Allow-Origin", "*");
-	    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	    next();
-	});
-	//@ set view engine
-	app.engine('html', require('ejs').renderFile);		
-	//@
-	app.set('view engine', 'html') ;
+  //@ allow/enable cross origin request
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+  //@ set view engine
+  app.engine("html", require("ejs").renderFile);
+  //@
+  app.set("view engine", "html");
 
-	//@ define static
-	app.use(express.static(path.resolve('views')));
+  //@ define static
+  app.use(express.static(path.resolve("views")));
 
-	//@ HTTP request logger
-	app.use(morgan('dev'));
-}
+  //@ HTTP request logger
+  app.use(morgan("dev"));
+};
