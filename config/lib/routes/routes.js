1987d1cca3130e4ba27587f6880557503c6fe021
KUMAR Jitendra <jitendra.kumar@soprasteria.com>
2019-01-31 11:58:58 +0530

    removed unused package from routes and code formatted

--- config/lib/routes/routes.js
+++ config/lib/routes/routes.js
@@ -1,8 +1,7 @@
-const path			     = require('path'),
-	fs 			         = require('fs'),
-    ENV                  = require(path.resolve(`./config/env/${process.env.NODE_ENV}`))
-    helperLib            = require(path.resolve('./config/lib/helper_lib')),
-	location		     = path.resolve('./modules');
+const path = require('path'),
+    fs = require('fs'),
+    helperLib = require(path.resolve('./config/lib/helper_lib')),
+    location = path.resolve('./modules');
 
 
 module.exports = (app) => {
@@ -11,22 +10,23 @@ module.exports = (app) => {
     fs.readdirSync(location)
         .filter((dir) => {
             return fs.statSync(`${location}/${dir}`).isDirectory();
-        }).forEach((dir, index) => {
-            let fileObj = require(path.resolve(`./modules/${dir}/routes/routes`));            
-            app.use(fileObj.base,	fileObj.router);
+        })
+        .forEach((dir) => {
+            let fileObj = require(path.resolve(`./modules/${dir}/routes/routes`));
+            app.use(fileObj.base, fileObj.router);
+        });
+
+
+    //@ global error handling middleware
+    app.use((err, req, res) => {
+
+        let Common = new helperLib.common.common();
+        let Middleware = new helperLib.middleware();
+
+        //@ write error logs into file
+        Middleware.writeErrorIntoFile(err, req);
+        let resObj = Common.generateResponses(500, 'failed', err.message || err.stack);
+
+        res.status(500).json(resObj);
     });
-
-
-    // //@ global error handling middleware
-    // app.use((err, req, res, next) => {
-
-    //     let Common        = new helperLib.common.common();
-    //     let Middleware    = new helperLib.middleware();
-
-    //     //@ write error logs into file
-    //     Middleware.writeErrorIntoFile(err, req);
-    //     let resObj = Common.generateResponses(500, 'failed', err.message || err.stack);
-
-    //     res.status(500).json(resObj);
-    // });    
 }
